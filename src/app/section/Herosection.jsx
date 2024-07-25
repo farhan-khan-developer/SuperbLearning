666c422f2383c11e1a6ab5523b3ed71f6fd8d174
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-07-26 01:30:19 +0530

    feat(Herosection): changed herosection style

--- src/app/section/Herosection.jsx
+++ src/app/section/Herosection.jsx
@@ -6,16 +6,9 @@ const Herosection = () => {
   return (
     <div className="flex justify-center items-center text-center text-[#160141] flex-col gap-5">
         <div className='flex flex-col'>
-            <div className='flex justify-center items-end'>
-                <h1 className='text-7xl'>Unlock</h1>
-                <Image
-                src={superblearningimg}
-                width={100}
-                height={100}
-                />
-                <h1 className='text-7xl'>the Future of </h1>
-            </div>
-            <p className='text-7xl'>Education with Our <br /> Comprehensive Training <br /> Programs!</p>
+            <p className='md:text-7xl xxsm:text-4xl'>
+                Unlock🔓the Future of <br /> Education with Our <br /> Comprehensive Training <br /> Programs!
+            </p>
         </div>
         <div>
             <h1 className='text-gray-400 text-2xl'>Ready to Empower Your Teaching Journey?</h1>
