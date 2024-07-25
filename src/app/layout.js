9e1e894a53eec11abbc29d6c3c604ae32dff60cc
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-07-25 15:17:01 +0530

    feat(layout): added new font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import { Concert_One } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const concert = Concert_One({subsets: ["latin"], weight: ['400']})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar/>
+        <div className={concert.className}>{children}</div>
+      </body>
     </html>
   );
 }
