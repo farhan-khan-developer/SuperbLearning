c41d54285d5e14f9cc968d6efa1f80d25842a8b4
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-07-27 13:47:22 +0530

    feat(layout): added navbar in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Concert_One } from "next/font/google";
+import { NavbarDemo } from "./components/NavbarDemo";
 
 const inter = Inter({ subsets: ["latin"] });
 const concert = Concert_One({subsets: ["latin"], weight: ['400']})
@@ -15,8 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar/>
-        <div className={concert.className}>{children}</div>
+        <NavbarDemo/>
+        <div className={`${concert.className} relative top-12`}>{children}</div>
       </body>
     </html>
   );
