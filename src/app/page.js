14b50cc3d274439745f206aeb72a44c63ed3b3d3
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-08-02 12:40:01 +0530

    feat(page): changed homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
-import Herosection from "./section/Herosection";
-import CoursesCards from "./section/CoursesCards";
+
 
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col gap-3">
-      <Herosection/>
-      <div className="md:visible xxsm:invisible">
-        <CoursesCards/>
-      </div>
+    <div>
+      <h1>Superb learning</h1>
     </div>
     </>
   );
