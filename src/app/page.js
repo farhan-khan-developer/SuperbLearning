38433be10f54c2a62640fe9b1ee3edbdb1f99c26
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-08-05 12:58:15 +0530

    feat(page): added new component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
+'use client'
+
+import Feature from "./components/Feature";
+import Herosection from "./components/Herosection";
 import Update from "./components/Update";
 
 
 export default function Home() {
   return (
     <>
-    <div>
+    <div className="flex flex-col gap-10">
       <Update/>
+      <Herosection/>
+      <Feature/>
     </div>
     </>
   );
