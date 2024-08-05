69cbd83d8ef523402147a22c9e45a9731f8a8541
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-08-05 13:00:54 +0530

    feat(Feature): created new component

---
+++ src/app/components/Feature.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StickyScrollRevealDemo } from './StickyScrollRevealDemo'
+
+const Feature = () => {
+  return (
+    <div className='h-screen  flex justify-center items-center'>
+      <StickyScrollRevealDemo/>
+    </div>
+  )
+}
+
+export default Feature
