2632551350673566e355d4b20845fdc9ad09e66e
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-08-07 11:48:17 +0530

    style(Herosection): changed style

--- src/app/components/Herosection.jsx
+++ src/app/components/Herosection.jsx
@@ -6,8 +6,8 @@ import Image from 'next/image'
 
 const Herosection = () => {
   return (
-    <div className='flex justify-center items-center p-10 flex-col gap-5'>
-      <h1 className='text-9xl font-light'>Superb Learning</h1>
+    <div className='flex justify-center items-center p-10 flex-col gap-7'>
+      <h1 className='text-9xl font-light'>Superb <span className='text-white bg-black p-2 rounded-lg '>Learning</span></h1>
       <div className='text-center flex gap-5 justify-center items-center'>
         <h2 className='text-4xl'>Authorized Center for Rural Urban <br /> Council of Skills & Vocational Studies
         </h2>
@@ -17,7 +17,7 @@ const Herosection = () => {
         height={100}
         />
       </div>
-      <button className='bg-[#0A1D58] px-10 py-3 rounded-xl text-white'>
+      <button className='bg-black px-10 py-3 rounded-xl text-white'>
         <p>Learn More</p>
       </button>
     </div>
