b283d59a806d01f05b7b67c889cc8617a481f09d
farhan-khan-developer <itsfarhankhan28@gmail.com>
2024-08-16 16:55:34 +0530

    feat(Offerings): imported new components

--- src/app/components/Offerings.jsx
+++ src/app/components/Offerings.jsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import React from "react";
 import { Card,Carousel } from "./ui/apple-cards-carousel";
+import { TabsDemo } from "./Tabs/TabsDemo";
+import { TabsDemoTwo } from "./Tabs/TabsDemoTwo";
+import { TabsDemoThree } from "./Tabs/TabsDemoThree";
 
 export const Offerings = ()=> {
   const cards = data.map((card, index) => (
@@ -10,7 +13,7 @@ export const Offerings = ()=> {
   ));
 
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center">
         <h2 className="text-5xl">Explore our Offerings.</h2>
         <Carousel items={cards} />
     </div>
@@ -18,34 +21,74 @@ export const Offerings = ()=> {
   );
 }
 
-const DummyContent = () => {
+const DummyContentOne = () => {
   return (
     <>
-      {[...new Array(3).fill(1)].map((_, index) => {
-        return (
-          <div
-            key={"dummy-content" + index}
-            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
-          >
-            <p className="max-w-3xl mx-auto font-sans text-base text-neutral-600 dark:text-neutral-400 md:text-2xl">
-              <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                The first rule of Apple club is that you boast about Apple club.
-              </span>{" "}
-              Keep a journal, quickly jot down a grocery list, and take amazing
-              class notes. Want to convert those notes to text? No problem.
-              Langotiya jeetu ka mara hua yaar is ready to capture every
-              thought.
-            </p>
-            <Image
-              src="/community.jpg"
-              alt="Macbook mockup from Aceternity UI"
-              height="500"
-              width="500"
-              className="object-contain w-full h-full mx-auto md:w-1/2 md:h-1/2"
-            />
+        <div
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+            <p className="max-w-3xl font-sans text-base text-neutral-600 dark:text-neutral-400 md:text-2xl">
+              At SUPERB LEARNING, a premier teacher training center, we are dedicated to fostering literacy and language skills through our comprehensive phonics training courses. Our expertly designed programs cater to the diverse needs of educators and young learners alike, ensuring a solid foundation in reading and writing.
+            </p>   
+        </div>
+        <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4 flex flex-col gap-5 text-neutral-600 dark:text-neutral-400">
+          <div className="max-w-3xl md:text-3xl">
+            <h1>Our phonics training is divided into two distinct courses:</h1>
           </div>
-        );
-      })}
+          <div>
+            <TabsDemo/>
+          </div>
+        </div>
+        <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4 flex flex-col gap-5 text-neutral-600 dark:text-neutral-400 md:text-2xl">
+        <p>Join us at SUPERB LEARNING and become a part of our mission to empower teachers and students with the tools they need to achieve academic excellence through effective phonics education.</p>
+        </div>
+    </>
+  );
+};
+
+const DummyContentTwo = () => {
+  return (
+    <>
+        <div
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+            <p className="max-w-3xl font-sans text-base text-neutral-600 dark:text-neutral-400 md:text-2xl">
+            At SUPERB LEARNING, we are dedicated to empowering educators with the skills and knowledge they need to excel in their careers. As an authorized center for the Rural and Urban Council of Skills and Vocational Studies, we offer a range of diploma courses designed to meet the growing demands of the education sector.
+            </p>   
+        </div>
+        <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4 flex flex-col gap-5 text-neutral-600 dark:text-neutral-400">
+          <div className="max-w-3xl md:text-3xl">
+            <h1>Our Diploma Courses:</h1>
+          </div>
+          <div>
+            <TabsDemoTwo/>
+          </div>
+        </div>
+        <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4 flex flex-col gap-5 text-neutral-600 dark:text-neutral-400 md:text-2xl">
+        <p>Join SUPERB LEARNING and take the first step towards a fulfilling career in education. Enroll in our diploma courses and become a part of our community dedicated to excellence in teaching and learning.</p>
+        </div>
+    </>
+  );
+};
+
+const DummyContentThree = () => {
+  return (
+    <>
+        <div
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+            <p className="max-w-3xl font-sans text-base text-neutral-600 dark:text-neutral-400 md:text-2xl">
+            At SUPERB LEARNING, an authorized center for RURAL AND URBAN COUNCIL OF SKILLS AND VOCATIONAL STUDIES, is dedicated to providing high-quality education and training to equip you with the skills needed to excel in your chosen career. We offer a variety of diploma courses designed to meet the needs of todays dynamic job market
+            </p>   
+        </div>
+        <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4 flex flex-col gap-5 text-neutral-600 dark:text-neutral-400">
+          <div className="max-w-3xl md:text-3xl">
+            <h1>Explore our courses below:</h1>
+          </div>
+          <div>
+            <TabsDemoThree/>
+          </div>
+        </div>
+        <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4 flex flex-col gap-5 text-neutral-600 dark:text-neutral-400 md:text-2xl">
+        <p>Join us at SUPERB LEARNING and become a part of our mission to empower teachers and students with the tools they need to achieve academic excellence through effective phonics education.</p>
+        </div>
     </>
   );
 };
@@ -53,21 +96,21 @@ const DummyContent = () => {
 const data = [
   {
     // category: "Artificial Intelligence",
-    title: "Phonics",
+    title: "PHONICS",
     src: "/phonics.jpg",
-    content: <DummyContent />,
+    content: <DummyContentOne />,
   },
   {
     // category: "Productivity",
-    title: "Teacher",
+    title: "TEACHER",
     src: "/teacher.jpg",
-    content: <DummyContent />,
+    content: <DummyContentTwo />,
   },
   {
     // category: "Product",
-    title: "Corporate",
+    title: "CORPORATE",
     src: "/corporate2img.png.jpg",
-    content: <DummyContent />,
+    content: <DummyContentThree />,
   },
 
 //   {
